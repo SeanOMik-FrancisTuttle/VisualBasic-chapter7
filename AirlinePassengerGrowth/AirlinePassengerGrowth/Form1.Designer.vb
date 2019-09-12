daba24b5a7759c504ef1db393608ea26178d1a94
SeanOMik <seanomik@gmail.com>
2019-09-12 10:08:36 -0500

    Forgot to add an assignment

---
+++ AirlinePassengerGrowth/AirlinePassengerGrowth/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGrowth = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstGrowth = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnYear = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.mtxtYear = New System.Windows.Forms.MaskedTextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnGrowth
+        '
+        Me.btnGrowth.Location = New System.Drawing.Point(19, 3)
+        Me.btnGrowth.Name = "btnGrowth"
+        Me.btnGrowth.Size = New System.Drawing.Size(115, 23)
+        Me.btnGrowth.TabIndex = 0
+        Me.btnGrowth.Text = "Calculate Growth"
+        Me.btnGrowth.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(266, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Arilines Passenger Growth"
+        '
+        'lstGrowth
+        '
+        Me.lstGrowth.FormattingEnabled = True
+        Me.lstGrowth.Location = New System.Drawing.Point(160, 37)
+        Me.lstGrowth.Name = "lstGrowth"
+        Me.lstGrowth.Size = New System.Drawing.Size(120, 134)
+        Me.lstGrowth.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(3, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(127, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Get amount for year:"
+        '
+        'btnYear
+        '
+        Me.btnYear.Location = New System.Drawing.Point(14, 86)
+        Me.btnYear.Name = "btnYear"
+        Me.btnYear.Size = New System.Drawing.Size(128, 23)
+        Me.btnYear.TabIndex = 5
+        Me.btnYear.Text = "Get Year Amount"
+        Me.btnYear.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Controls.Add(Me.mtxtYear)
+        Me.Panel1.Controls.Add(Me.lblYear)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.btnYear)
+        Me.Panel1.Controls.Add(Me.btnGrowth)
+        Me.Panel1.Location = New System.Drawing.Point(3, 37)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(152, 134)
+        Me.Panel1.TabIndex = 6
+        '
+        'lblYear
+        '
+        Me.lblYear.BackColor = System.Drawing.Color.Transparent
+        Me.lblYear.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.lblYear.Location = New System.Drawing.Point(0, 111)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(152, 23)
+        Me.lblYear.TabIndex = 6
+        Me.lblYear.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'mtxtYear
+        '
+        Me.mtxtYear.Location = New System.Drawing.Point(127, 61)
+        Me.mtxtYear.Mask = "00"
+        Me.mtxtYear.Name = "mtxtYear"
+        Me.mtxtYear.Size = New System.Drawing.Size(18, 20)
+        Me.mtxtYear.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(288, 176)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lstGrowth)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = " Airline Passenger Growth"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGrowth As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lstGrowth As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnYear As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblYear As Label
+    Friend WithEvents mtxtYear As MaskedTextBox
+End Class
---
+++ AirlinePassengerGrowth/AirlinePassengerGrowth/Form1.vb
@@ -0,0 +1,69 @@
+﻿' Program Name: Airlines Passenger Growth
+' Author:       Sean Ervin
+' Date:         9/12/2019
+' Purpose:      Computes the next ten years of passengers that will fly on planes, or a 
+'               specified year given by the user.
+
+Public Class Form1
+    ' This was the amount of passengers in the year 2017.
+    Dim int2017Passengers As Long = 3800000000
+    ' This was the calculated yearly growth.
+    Dim decYearlyGrowth As Decimal = 0.069
+
+    Private Sub BtnGrowth_Click(sender As Object, e As EventArgs) Handles btnGrowth.Click
+        ' This executes a sub procedure to get the next ten years passengers and puts it to the list.
+        GetTenYearGrowth()
+    End Sub
+
+    Private Sub GetTenYearGrowth()
+        ' This gets the next ten years growth and inputs it to the list.
+
+        lstGrowth.Items.Clear()
+        Dim intLastYearPass As Long = int2017Passengers
+        For i As Integer = 1 To 10
+            intLastYearPass += intLastYearPass * decYearlyGrowth
+            lstGrowth.Items.Add("Year " & i & ": " & intLastYearPass.ToString())
+        Next
+    End Sub
+
+
+    Private Sub BtnYear_Click(sender As Object, e As EventArgs) Handles btnYear.Click
+        ' This validates the years text and if its valid, then it shows the inputed year's passgener count.
+        If ValidiateYearText() Then
+            GetYearGrowth()
+        End If
+    End Sub
+
+    Private Sub GetYearGrowth()
+        ' This gets a specific years amount of passengers.
+        Dim intLastYearPass As Long = int2017Passengers
+        For i As Integer = 1 To Convert.ToInt32(mtxtYear.Text)
+            intLastYearPass += intLastYearPass * decYearlyGrowth
+        Next
+
+        ' Display the text
+        lblYear.Text = "Year " & Convert.ToInt32(mtxtYear.Text) & ": " & intLastYearPass.ToString()
+    End Sub
+
+    Private Function ValidiateYearText() As Boolean
+        ' This validiates the text to make sure it is a number and is in the next 10 years.
+        Try
+            Dim intYear As Integer = Convert.ToInt32(mtxtYear.Text)
+            ' Check if in the next 10 years
+            If (intYear > 0 And intYear <= 10) Then
+                Return True
+            Else
+                MsgBox("Enter a number for a year in the next 10 years", , "Error")
+                Return False
+            End If
+        Catch ex As Exception
+            MsgBox("Enter a number for a year in the next 10 years", , "Error")
+            Return False
+        End Try
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Wait for two seconds to show the splash screen
+        Threading.Thread.Sleep(2000)
+    End Sub
+End Class
